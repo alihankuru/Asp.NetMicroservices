03ecb22f5d03fb13b939d0ba71cf8133075b034d
AK <ali.kuru6@hotmail.com>
2023-08-11 00:14:08 +0300

    added docker compose for windows but maybe change linux.

--- src/Services/Catolog/Catolog.API/Program.cs
+++ src/Services/Catolog/Catolog.API/Program.cs
@@ -1,4 +1,5 @@
 using Catolog.API.Data;
+using Catolog.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //Register Serices
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
